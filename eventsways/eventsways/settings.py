a4b691185713320df0079fd67057e1001434d106
Krutolevich Siamion <semasolyara@gmail.com>
2020-01-22 16:43:59 +0300

    static

--- eventsways/eventsways/settings.py
+++ eventsways/eventsways/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eventsways.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'eventsways/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/WildSandals/eventsways/static/',
+)
 STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
