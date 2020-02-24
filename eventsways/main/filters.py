9a6bca8a0335ba47d44b6ac72e13d059000a27f6
Krutolevich Siamion <semasolyara@gmail.com>
2020-02-24 13:34:05 +0300

    LASTEBANYIFILTER

--- eventsways/main/filters.py
+++ eventsways/main/filters.py
@@ -1,12 +1,17 @@
 import django_filters
 from django_filters.widgets import RangeWidget, SuffixedMultiWidget, CSVWidget
-from .models import Event, Category
+from .models import Event, Category, Address
 from django import forms
+from django.forms.widgets import ChoiceWidget, Select
 
 class EventFilter(django_filters.FilterSet):
     events_holding_date = django_filters.DateFromToRangeFilter(widget=RangeWidget(attrs={'type': 'date','placeholder': 'YYYY/MM/DD',}))
-    category = django_filters.ModelMultipleChoiceFilter(queryset=Category.objects.all(),widget= forms.CheckboxSelectMultiple)
+    category = django_filters.ModelMultipleChoiceFilter(queryset=Category.objects.all(),widget = forms.CheckboxSelectMultiple)
+    address__country = django_filters.ChoiceFilter(choices=[(country, country) for country in Address.objects.all().values_list('country', flat=True).distinct()])
 
     class Meta:
         model = Event
-        fields = ['events_holding_date', 'category']
+        fields = ['events_holding_date', 'category', 'address__country']
+        widgets = {
+              'address__country': Select(attrs={'type': 'combobox'}),
+          }
