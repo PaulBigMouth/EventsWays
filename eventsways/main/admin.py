9359d044f6c03f3a3fe1efc49b156817d3f93105
Krutolevich Siamion <semasolyara@gmail.com>
2020-02-25 13:13:49 +0300

    ghrwhg321431

--- eventsways/main/admin.py
+++ eventsways/main/admin.py
@@ -24,14 +24,33 @@ class EventAdmin(admin.ModelAdmin):
     search_fields = ('title', 'body', 'category__title',)
     save_as = True
     readonly_fields = ('get_image',)
-
+    list_editable = ("checked",)
+    save_on_top = True
+    actions = ["published", "unpublished"]
     form = EventAdminForm
 
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.events_image.url} width="50px" height="60px"')
 
     get_image.short_description = "Изображение"
-    
+
+    def unpublished(self, request, queryset):
+        row_update = queryset.update(checked=True)
+
+        if row_update == "1":
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+
+    def published(self, request, queryset):
+        row_update = queryset.update(checked=False)
+
+        if row_update == "1":
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+
+        self.message_user(request, f"{message_bit}")    
 
 admin.site.site_title = 'EventsWays'
 admin.site.site_header = 'EventsWays'
