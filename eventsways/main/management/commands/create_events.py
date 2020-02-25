8b96700291251e2d874b3e04155f85f709ac61ce
Krutolevich Siamion <semasolyara@gmail.com>
2020-02-26 02:42:29 +0300

    generate_events

--- eventsways/main/management/commands/create_events.py
+++ eventsways/main/management/commands/create_events.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from main.models import Event, Category, Address
+from main.models import Event, Category
 import datetime
 import random
 import time
@@ -7,17 +7,17 @@ from datetime import datetime as dt
 
 
 
-categories = ['Sport','LifeStyle','Misic','Travelling']
+categories = ['Спорт','Стиль Жизни','Музыка','Путешествия']
 
-locations = ['National Library', 'Bar 1703', 'Markos', 'Hostel in Gagrina']
+place = ['Национальная библиотека', 'Bar 1703', 'ТЦ Пирамида', 'Отель ТётяМарина228']
 
-street = ['Lenina', 'Gagarina', 'Mayakovskogo', 'Masherova']
+street = ['Ленина', 'Гагарина', 'Маяковского', 'Машерова']
 
-city = ['Horki','Vitebsk','Slutsk', 'Minsk']
+city = ['Горки','Витебск','Слуцк', 'Минск']
 
-region = ['Vitebskaya obl', 'Minskaya obl', 'Brestskaya obl', 'Mohilevskaya obl']
+region = ['Витебская область', 'Минская область', 'Брестская область', 'Могилёвская область']
 
-country = ['Belarus', 'Russia', 'Ukraine', 'Kazakhstan']
+country = ['Беларусь', 'Россия', 'Украина', 'Казахстан']
 
 def generate_holding_date():
     year = random.randint(2000, 2030)
@@ -35,29 +35,33 @@ def generate_category_title():
     index = random.randint(0,3)
     return categories[index]
 
-def generate_locations():
+def generate_street_and_premis():
+    index_street = random.randint(0,500)
     index = random.randint(0,3)
-    return locations[index]
+    return (street[index] + ' кв ' + str(index_street))
 
-def generate_premises():
-    index = random.randint(0,500)
-    return (str(index) + ' kv')
-
-def generate_street():
+def generate_place():
     index = random.randint(0,3)
-    return street[index]
+    return place[index]
+
+
+def generate_lng_and_lat():
+    lngfloat = random.random()
+    lngint = random.randint(1,100)
+    return (float(lngfloat+lngint)) 
+
+def generate_checked():
+    checked = random.choice([True, False])
+    return checked
 
 def generate_city():
     index = random.randint(0,3)
     return city[index]
 
-def generate_region():
-    index = random.randint(0,3)
-    return region[index]
-
 def generate_country():
     index = random.randint(0,3)
     return country[index]
+
 """
 def generate_category_slug():
     now = dt.now()
@@ -80,26 +84,19 @@ class Command(BaseCommand):
                 events_holding_date = generate_holding_date()
                 events_holding_time = generate_holding_time()
                 category_title = generate_category_title()
-                location = generate_locations()
-                premises = generate_premises()
-                street = generate_street()
+                street_and_premis = generate_street_and_premis()
                 city = generate_city()
-                region = generate_region()
+                place = generate_place()
                 country = generate_country()
+                lng = generate_lng_and_lat()
+                lat = generate_lng_and_lat()
+                checked = generate_checked()
+                email = (row + '@gmail.com')
 
-
-
+                
                 category = Category.objects.get_or_create(
                     title = category_title,
                     slug = category_title,
-                )
-
-                address = Address.objects.get_or_create(
-                    premises = premises,
-                    street = street,
-                    city = city,
-                    region = region,
-                    country = country
                 )
 
                 event = Event.objects.get_or_create(
@@ -108,9 +105,14 @@ class Command(BaseCommand):
                     events_holding_date = events_holding_date,
                     events_holding_time = events_holding_time,
                     category = Category.objects.get(title=category_title),
-                    location = location,
-                    address = Address.objects.get(premises=premises, city = city, street = street, region = region, country = country),
-                    )
-
+                    lng = lng,
+                    lat = lat,
+                    place = place,
+                    street_and_premis = street_and_premis,
+                    city = city,
+                    country = country,
+                    checked = checked,
+                    email = email,
+                )
 
         self.stdout.write(self.style.SUCCESS('Data imported successfully'))
